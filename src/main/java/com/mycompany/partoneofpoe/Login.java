3b2e84cb20544fdf2679ba88deea803becd4f335
RC_Student_lab <RC_Student_lab@10.0.0.148>
2024-09-30 16:35:51 +0000

    Creating a Login() method and declaring attributes

---
+++ src/main/java/com/mycompany/partoneofpoe/Login.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.partoneofpoe;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class Login {
+    
+    //attributes
+    private String userName;
+    private String password;
+    private String firstName;
+    private String lastName;
+    
+}
